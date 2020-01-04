af175988c7b97d167db5c3df566eb949021cd39a
SirAdrian <radoiadrian77@gmail.com>
2020-01-04 16:29:22 +0200

    Homowork

--- src/com/company/BankAccount.java
+++ src/com/company/BankAccount.java
@@ -1,137 +1,76 @@
-import java.util.Scanner;
-public class BankAccount {
+package com.company;
 
-    public BankAccount(String rva, String ro0001) {
+        public class Account {
 
-    }
+            String name;
+            int balance = 10000;
 
-    public static void main(String[] args) {
-        // TODO  Auto generator method stub
+            //construct
+            public Account(String myName, int myBalance) {
+                this.name = myName;
+                this.balance = myBalance;
+            }
 
-        BankAccount obj1 = new BankAccount("RVA", "RO0001");
+            public String getName() {
+                return name;
+            }
+
+            public int getBalance() {
+                return balance;
+            }
+
+            public String displayBalance(int pinNumber) {
+                if (pinNumber == myPinCode()) {
+                    return "Your balance is " + balance + " euro";
+                } else {
+                    return pinError();
+                }
+            }
+
+            public String deposite(int pinNumber, int amount) {
+                if (pinNumber == myPinCode()) {
+                    balance += amount;
+                    return "you deposite " + amount + " and your balance is now " + balance + " euro";
+                } else {
+                    return pinError();
+                }
+
+            }
+
+            public String withdraw(int pinNumber, int amount) {
+                if (pinNumber == myPinCode()) {
+                    balance -= amount;
+                    return "you withdraw " + amount + " and your balance is now " + balance + " euro";
+                } else {
+                    return pinError();
+                }
+
+            }
+
+            private int myPinCode() {
+                int myPin = 1234;
+                return myPin;
+            }
+
+            private String pinError() {
+                String errorMessage = "You have enter the rong pin number!";
+                return errorMessage;
+            }
 
 
-    }
-}
+            public static void main(String[] args) {
+                com.company.Account person1 = new com.company.Account("Emerson", 10000);
+                String myName = person1.getName();
+                String myAccountBalance = person1.displayBalance(1234);
+                String newAccountBalance = person1.deposite(1234, 500);
+                String withdrawFromAccount = person1.withdraw(1234, 250);
 
-class BankAccount
-{
+                System.out.println(myName);
+                System.out.println(newAccountBalance);
+                System.out.println(withdrawFromAccount);
+            }
 
-    int balance;
-    int previousTransactions;
-    String customerName;
-    String customerId;
-
-
-    BankAccount(String cname,String cid)
-    {
-        customerName = cname;
-        customerId = cid;
-    }
-
-
-
-     deposit(int amount)
-    {
-        if(amount !=0)
-        {
-            int balace = balace + amount;
         }
-    }
-
-     witdraw(int amount)
-    {
-        if (amount !=0)
-        {
-            balance = balance - amount;
-            previousTransactions = -amount;
-
-        }
-    }
-    void getPreviousTransactions()
-    {
-        if (previousTransactions > 0)
-            System.out.println("Deposited: "+previousTransactions);
-    }
-
-    /**
-     * @param previousTransactions
-     */
-    public void setPreviousTransactions(int previousTransactions) {
-        this.previousTransactions = previousTransactions;
-    }
-     if(previosTransaction < 0),
-}
-    System.out.println("Withdraw: "+Math.abs(previosTransaction));
-            }
-            else
-            {
-            System.out.println("No transaction occured");
-            }
-            }
-            void
-            {
-            char option='/0';
-            Scanner scanner = new Scanner(System.in);
-
-            System.out.println("Welcome "+customerName);
-            System.out.println("Your ID is" + customerId);
-            System.out.println("/n");
-            System.out.println("A. Check Balace");
-            System.out.println("B. Deposit");
-            System.out.println("C. Withdraw");
-            System.out.println("D. Previos transaction");
-            System.out.println("E. Exit");
-
-            do
-            {
-                System.out.println("-----------------------------------------")
-                System.out.println("Enter an option");
-                System.out.println("=========================================")
-            option = scaner.next().charAt(0);
-                System.out.println("/n");
-
-                switch(option)
-            {
-
-                case 'A':
-                    System.out.println("-------------");
-                    System.out.println("Balace = "+balace);
-                    System.out.println("--------------");
-                    System.out.println("/n");
-
-                    case 'B':
-            System.out.println("-------------");
-            System.out.println("Enter an amount to deposit);
-            System.out.println("--------------");
-            System.out.println("/n");
-            int amount = scanner.nextInt();
-            deposit(amount);
-            System.out.println("/n");
-            break;
-
-
-            case 'C':
-            System.out.println("-------------");
-            System.out.println("Enter an amount to withdraw:" );
-            System.out.println("--------------");
-            int amount2 = scanner.nextInt();
-            deposit(amount2);
-            System.out.println("/n");
-            break;
-
-
-
-            case 'E':
-                System.out.println("*********************");
-                break;
-
-
-
-{
-
-
-
 
 
 
